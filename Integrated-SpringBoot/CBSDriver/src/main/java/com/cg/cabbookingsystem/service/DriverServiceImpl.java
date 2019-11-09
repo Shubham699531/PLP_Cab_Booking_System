8337c86956ef61869d1be8ce6adf8f511dccf6d1
Shubham699531 <33036752+Shubham699531@users.noreply.github.com>
2019-11-10 00:33:30 +0530

    Update DriverServiceImpl.java

--- Integrated-SpringBoot/CBSDriver/src/main/java/com/cg/cabbookingsystem/service/DriverServiceImpl.java
+++ Integrated-SpringBoot/CBSDriver/src/main/java/com/cg/cabbookingsystem/service/DriverServiceImpl.java
@@ -13,6 +13,12 @@ import com.cg.cabbookingsystem.dto.Driver;
 import com.cg.cabbookingsystem.dto.Vehicle;
 import com.cg.cabbookingsystem.repo.DriverRepo;
 
+/**
+ * Implementation of roles of a driver
+ * @author Shubham
+ * @version 1.0
+ *
+ */
 @Transactional
 @Service
 public class DriverServiceImpl implements DriverService{
@@ -40,9 +46,11 @@ public class DriverServiceImpl implements DriverService{
 	 * 	id of the driver who is logged in
 	 * @return
 	 * list of past rides along with source, destination and fare
+	 * @throws NoPastRidesFoundException 
+	 * if no past rides exist for the driver with the queried driverId
 	 */
 	@Override
-	public List<Booking> getAllTripsOfADriver(int userId) {
+	public List<Booking> getAllTripsOfADriver(int userId) throws NoPastRidesFoundException{
 		return repo.getAllTripsOfADriver(userId);
 	}
 
@@ -64,14 +72,24 @@ public class DriverServiceImpl implements DriverService{
 	 * id of the driver who is logged in
 	 * @return
 	 * customer details who is alloted to the logged in driver
+	 * @throws NoBookingFoundException 
+	 * if there's no booking for the driver with the given driverId
 	 */
 	@Override
-	public Customer searchForBooking(int driverId) {
+	public Customer searchForBooking(int driverId) throws NoBookingFoundException {
 		return repo.searchForBooking(driverId);
 	}
-	
+	/**
+	 * Gets booking details for a particular driver
+	 * @param driverId
+	 * id of the driver whose booking details are to be fetched
+	 * @return
+	 * confirmed booking details for the driver
+	 * @throws NoBookingFoundException 
+	 * if there's no booking for the driver with the given driverId
+	 */
 	@Override
-	public Booking getBookingDetailsForADriver(int driverId) {
+	public Booking getBookingDetailsForADriver(int driverId) throws NoBookingFoundException{
 		return repo.getBookingDetailsForADriver(driverId);
 	}
 
