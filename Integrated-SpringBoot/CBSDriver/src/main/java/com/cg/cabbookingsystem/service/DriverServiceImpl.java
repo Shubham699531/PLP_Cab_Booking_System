a51e30f5491008a347e9cac6aa059b9a2d54821b
Shubham699531 <33036752+Shubham699531@users.noreply.github.com>
2019-11-10 00:37:13 +0530

    Update DriverServiceImpl.java

--- Integrated-SpringBoot/CBSDriver/src/main/java/com/cg/cabbookingsystem/service/DriverServiceImpl.java
+++ Integrated-SpringBoot/CBSDriver/src/main/java/com/cg/cabbookingsystem/service/DriverServiceImpl.java
@@ -11,6 +11,8 @@ import com.cg.cabbookingsystem.dto.Booking;
 import com.cg.cabbookingsystem.dto.Customer;
 import com.cg.cabbookingsystem.dto.Driver;
 import com.cg.cabbookingsystem.dto.Vehicle;
+import com.cg.cabbookingsystem.exception.NoBookingFoundException;
+import com.cg.cabbookingsystem.exception.NoPastRidesFoundException;
 import com.cg.cabbookingsystem.repo.DriverRepo;
 
 /**
